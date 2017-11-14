967187970a46834950a8e890dc640145657e183f
Jace P. Gold <Jace@JaceGold.com>
2017-11-13 17:30:03 -0700

    Add ability to calculate wins/losses in Craps

--- craps.rb
+++ craps.rb
@@ -1,3 +1,9 @@
+# To Do
+# Gameplay menu
+# Add money
+# Subtract Money
+
+
 require 'colorize'
 require 'pry'
 require_relative 'player'
@@ -10,6 +16,8 @@ class Craps
     @player = player
     puts "----- Let\'s play Craps, #{@player.name}! -----"
    @player.check_amount
+   @win_amount = 0
+   @loss_amount = 0
     menu
   end
 
@@ -17,10 +25,8 @@ class Craps
     @first_roll = 0
     @roll = 0
     @point = 0
-    @win_amount = 0
-    @loss_amount = 0
     # @first_roll = @dice.roll.show_dice # Roll the Dice
-    puts 'Press Enter/Return to Roll the Dice.'
+    puts 'Press Enter/Return to Roll the Dice.'.colorize(:light_blue)
     input = $stdin.gets.strip
     @first_roll = @dice.roll
     puts "First Roll = #{@first_roll}."
@@ -31,32 +37,36 @@ class Craps
       game
     elsif @first_roll == 2 || @first_roll == 3 || @first_roll == 12
       puts "Player loses"
-      @loss_amount = @loss_amount + 1
+      @loss_amount  += 1
       game
     else
       @point = @first_roll
       puts "The point is #{@point}"
       is_won = false
+      puts "First roll is done. Now onto the challenging part.\n\n"
       secondary_loop
     end
   end
 
 def secondary_loop
-  puts 'Press Enter/Return to roll the dice.'
+  puts 'Press Enter/Return to roll the dice.'.colorize(:light_blue)
+  puts "The Point is #{@point}!".colorize(:yellow)
   input = $stdin.gets.strip
   while !@is_won
     @roll = @dice.roll
     if @roll == @point
       puts "Rolled a #{@roll}."
       puts "Player wins!"
+      puts "House looses."
       @win_amount += 1
       secondary_loop
     else
       if @roll == 7
         puts "Rolled a #{@roll}."
         puts 'Player loses...'
-        @loss_amount += 1
+        @loss_amount +=  1
         isWon = true
+        puts "House wins!"
         user_stats
       end
     end
@@ -64,10 +74,9 @@ def secondary_loop
 end
 
   def user_stats
-    puts 'This is the user stats function'
-    # Create code to count how many wins & loses the player has
-    puts "Wins: #{@win_amount}."
-    puts "Losses: #{@loss_amount}."
+    puts 'User Win/Lose Stats'
+    puts "Wins: #{@win_amount}.".colorize(:green)
+    puts "Losses: #{@loss_amount}.".colorize(:red)
     menu
   end
   end
@@ -81,7 +90,7 @@ end
   def menu
     puts '1.) To Play the game, press 1'
     puts '2.) To View player stats, press 2'
-    puts '3.) To Exit the game, press 4'
+    puts '3.) To Exit the game, press 3'
     print '> '
     selection = $stdin.gets.strip.downcase
 
