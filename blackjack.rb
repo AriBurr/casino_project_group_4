ae6c53b7bb0c3ef47072b0657ee7d8af512a6dd9
Justin Dean <deanplums@gmail.com>
2017-11-13 21:52:56 -0700

    deals but no go

--- blackjack.rb
+++ blackjack.rb
@@ -8,12 +8,13 @@ require_relative 'deck'
 
 
 class Blackjack
-  attr_accessor :player
+  attr_accessor :player, :deck
   def initialize(player)
     @player = player
     @deck = Deck.new
 
     init_blackjack()
+    init_blackjack2
   end
 
   def init_blackjack()
@@ -25,21 +26,16 @@ blackjack_menu()
 end
 
 def blackjack_menu()
-  puts = "Rules - Deal, decide--hit or stay---over 21 is a bust"
-  menu_options = ["Deal", "Hit me!", "Back to Casino"]
+  puts "Rules - Deal, then decide to hit or stay---over 21 is a bust"
+  menu_options = ["Deal", "Back to Casino"]
   #puts = "Rules are: Deal, hit but you must stay below 21 or you will bust!"
   menu_options.each_with_index { |opt, i| puts "[#{i + 1}] #{opt}" }
   #puts = menu_options
-action = gets.strip.to_i
+  action = gets.strip.to_i
 case action
   when 1
-    blackjack_rules()
-  when 2
     deal()
-#binding.pry
-  when 3
-    hit()
-  when 4
+  when 2
     return
   else
     puts "Invalid Input -- Please Try Again"
@@ -51,17 +47,50 @@ puts "==========================".yellow
 puts "| Deal - $5 per hand |".yellow
 puts "==========================".yellow
   end
-#binding.pry
-end
-player = Player.new()
-Blackjack.new(player)
-#Deck.new()
-#Card.new()
+
 # Deal two cards
 #binding.pry
 def deal()
-  @deck.shuffle
+#binding.pry
+  shuffled = @deck.shuffle_cards
+  #binding.pry
+  dealer_cards = shuffled.shift(2)
+  user_cards = shuffled.shift(2)
+  #binding.pry
+  puts "Dealer hand #{dealer_cards[0].rank} of #{dealer_cards[0].suit} and #{dealer_cards[1].rank} of #{dealer_cards[1].suit}"
+  puts "Your hand #{user_cards[0].rank} of #{user_cards[0].suit} and #{user_cards[1].rank} of #{user_cards[1].suit}"
+
 end
-# wait for hit or stay
-# deal one card
-# evaluate whether the combination of cards is above 21
+
+def init_blackjack2()
+  puts "Now you must decide to stay or hit".yellow
+#binding.pry
+blackjack_menu2()
+end
+
+  def blackjack_menu2()
+    puts "Hit or Stay--remember money is riding on this"
+    menu_options = ["Hit", "Stay"]
+    menu_options.each_with_index { |opt, i| puts "[#{i + 1}] #{opt}"}
+      #puts = menu_options
+  action = gets.strip.to_i
+  case action
+    when 1
+      hit()
+    when 2
+  #stay--write code to add up and present results
+    when 4
+      return
+    else
+      puts "Invalid Input -- Please Try Again"
+    end
+end
+  def hit()
+    shuffled = @deck.shuffle_cards
+    dealer_cards = shuffled.shift(1)
+    user_cards = shuffled.shift(1)
+end
+end
+
+player = Player.new()
+Blackjack.new(player)
